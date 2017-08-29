8b45b0afa0c74c0b70febf1b24266ce40be9c599
Yudi Tan <yudi98@berkeley.edu>
2017-08-28 17:50:44 -0700

    updated .gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase';
-const config = {
-    apiKey: "AIzaSyAr6ABsEFegsSDQZqxZmeTH_0JVAz5Omhs",
-    authDomain: "bear-schedules.firebaseapp.com",
-    databaseURL: "https://bear-schedules.firebaseio.com",
-    projectId: "bear-schedules",
-    storageBucket: "",
-    messagingSenderId: "393933232888"
-  };
+import config from './firebaseconfig.js';
+
   firebase.initializeApp(config);
   export const provider = new firebase.auth.GoogleAuthProvider();
   export const auth = firebase.auth();
